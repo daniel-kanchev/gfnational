9c8b1f157f9c74eb7de22ca1d72b993bd5314217
daniel-kanchev <daniel.kanchev@adata.pro>
2021-03-25 14:25:05 +0200

    Finished Script

---
+++ README.md
@@ -0,0 +1,11 @@
+URL: https://www.gfnational.com/About/In-The-News
+
+    Spider name: gfnational
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Notes:
---
+++ gfnational/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'gfnational'
+SPIDER_MODULES = ['gfnational.spiders']
+NEWSPIDER_MODULE = 'gfnational.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'gfnational.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
